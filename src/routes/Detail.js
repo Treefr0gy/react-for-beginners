92b009e00813bbae8a6a1adf68b41d59acd74bdf
이준성 <109706353+Treefr0gy@users.noreply.github.com>
2025-04-03 12:55:44 +0900

    Update Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+
+  if (movie) {
+    return <span>{movie.title}</span>;
+  } else {
+    return null;
+  }
 }
+
 export default Detail;
